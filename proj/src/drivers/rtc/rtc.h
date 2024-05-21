70917ade02db85a0b5d0772f35b50293b7c0d260
Sara Cortez <up202205636@g.uporto.pt>
2024-05-21 15:15:28 +0100

    rtc inspired by labs

---
+++ proj/src/drivers/rtc/rtc.h
@@ -0,0 +1,43 @@
+#ifndef _LCOM_RTC_H_
+#define _LCOM_RTC_H_
+
+#include <minix/sysutil.h>
+#include <lcom/lcf.h>
+
+#define ISUPDATING 0x10000000
+#define ISBIN  0x100
+
+#define INPUT_REG      0x70
+#define OUTPUT_REG     0x71
+#define RTC_IRQ_LINE   8
+
+#define UPDATE_REG 10
+#define COUNT_REG   11
+
+#define H   4 
+#define M   2
+#define S   0  
+#define Y   9
+#define M   8
+#define D   7
+
+
+typedef struct {
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} datetime_info;
+
+uint8_t to_bin(uint8_t bcd_number);
+void setup_rtc();
+int subscribe_int_rtc();
+int unsubscribe_int_rtc();
+int output_rtc(uint8_t command, uint8_t *output);
+int updating_rtc();
+int is_bin_rtc();
+int update_time_rtc();
+
+#endif
