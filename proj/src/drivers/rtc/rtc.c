e49f2a1dc956ba076df98de7011805355ff079ec
Sara Cortez <up202205636@g.uporto.pt>
2024-05-22 11:09:46 +0100

    rtc

--- proj/src/drivers/rtc/rtc.c
+++ proj/src/drivers/rtc/rtc.c
@@ -2,42 +2,33 @@
 
 datetime_info time_info;
 int rtc_hook_id = 5; 
-uint8_t bin_mode;        
+uint8_t bin_mode;   
 
-int is_bin_rtc() {
-    uint8_t ts;
-    if (output_rtc(COUNT_REG, &ts)) return 1;
-	return ISBIN & ts;
-}
 
 int update_time_rtc() {
     
     if (rtc_is_updating() != 0) return 1;
     uint8_t output;
-
     if (output_rtc(Y, &output) != 0) return 1;
     time_info.year = bin_mode ? output : to_bin(output);
-
     if (output_rtc(M, &output) != 0) return 1;
     time_info.month = bin_mode ? output : to_bin(output);   
-
     if (output_rtc(D, &output) != 0) return 1;
     time_info.day = bin_mode ? output : to_bin(output);
-
     if (output_rtc(H, &output) != 0) return 1;
     time_info.hours = bin_mode ? output : to_bin(output);
-
     if (output_rtc(M, &output) != 0) return 1;
     time_info.minutes = bin_mode ? output : to_bin(output);
-
     if (output_rtc(S, &output) != 0) return 1;
     time_info.seconds = bin_mode ? output : to_bin(output);
-
     return 0;
 }
 
 void setup_rtc() {
-    bin_mode = rtc_is_bin();
+    uint8_t ts;
+    if (output_rtc(COUNT_REG, &ts)) return 1;
+	if(ISBIN & ts)
+        bin_mode = 1;
     update_time_rtc();
 }
 
@@ -54,8 +45,8 @@ int output_rtc(uint8_t cmd, uint8_t *output) {
         uint32_t bits32;                   
         if(sys_inb(cmd, &bits32)!=0) return 1;  
         *output = 0xFF & bits32;    
-    if (sys_outb(INPUT_REG, cmd) != 0) return 1;
-	return (util_sys_inb(OUTPUT_REG, output) != 0);
+        if (sys_outb(INPUT_REG, cmd) != 0) return 1;
+        return (util_sys_inb(OUTPUT_REG, output) != 0);
 }
 
 int updating_rtc(){
@@ -63,8 +54,6 @@ int updating_rtc(){
     if (output_rtc(UPDATE_REG, &ts)) return 1;
 	return ISUPDATING & ts;
 }
-
-
 
 uint8_t to_bin(uint8_t inbcd) {
     return ((inbcd >> 4) * 10) + (inbcd & 0xF);
