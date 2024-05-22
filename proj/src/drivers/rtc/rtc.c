02f5adcfd04636d18d7e5bed3b64d36ee6d93171
Sara Cortez <up202205636@g.uporto.pt>
2024-05-22 10:52:45 +0100

    rtc

--- proj/src/drivers/rtc/rtc.c
+++ proj/src/drivers/rtc/rtc.c
@@ -16,19 +16,19 @@ int update_time_rtc() {
     uint8_t output;
 
     if (output_rtc(Y, &output) != 0) return 1;
-    time_info.year = bin_mode ? output : to_binary(output);
+    time_info.year = bin_mode ? output : to_bin(output);
 
     if (output_rtc(M, &output) != 0) return 1;
-    time_info.month = bin_mode ? output : to_binary(output);   
+    time_info.month = bin_mode ? output : to_bin(output);   
 
     if (output_rtc(D, &output) != 0) return 1;
-    time_info.day = bin_mode ? output : to_binary(output);
+    time_info.day = bin_mode ? output : to_bin(output);
 
     if (output_rtc(H, &output) != 0) return 1;
-    time_info.hours = bin_mode ? output : to_binary(output);
+    time_info.hours = bin_mode ? output : to_bin(output);
 
     if (output_rtc(M, &output) != 0) return 1;
-    time_info.minutes = bin_mode ? output : to_binary(output);
+    time_info.minutes = bin_mode ? output : to_bin(output);
 
     if (output_rtc(S, &output) != 0) return 1;
     time_info.seconds = bin_mode ? output : to_bin(output);
@@ -41,18 +41,21 @@ void setup_rtc() {
     update_time_rtc();
 }
 
-int unsubscribe_int_rtc() {
+int subscribe_int_rtc() {
     return sys_irqsetpolicy(RTC_IRQ_LINE, IRQ_REENABLE, &rtc_hook_id);
 }
 
-int subscribe_int_rtc() {
+int unsubscribe_int_rtc() {
     return sys_irqrmpolicy(&rtc_hook_id);
 }
 
 int output_rtc(uint8_t cmd, uint8_t *output) {
+        if (output == NULL){ printf("failed to read undefined value"); return 1; }
+        uint32_t bits32;                   
+        if(sys_inb(cmd, &bits32)!=0) return 1;  
+        *output = 0xFF & bits32;    
     if (sys_outb(INPUT_REG, cmd) != 0) return 1;
-	if (util_sys_inb(OUTPUT_REG, output) != 0) return 1;
-    return 0;
+	return (util_sys_inb(OUTPUT_REG, output) != 0);
 }
 
 int updating_rtc(){
@@ -63,7 +66,7 @@ int updating_rtc(){
 
 
 
-uint8_t to_bin(uint8_t bcd_number) {
-    return ((bcd_number >> 4) * 10) + (bcd_number & 0xF);
+uint8_t to_bin(uint8_t inbcd) {
+    return ((inbcd >> 4) * 10) + (inbcd & 0xF);
 }
 
