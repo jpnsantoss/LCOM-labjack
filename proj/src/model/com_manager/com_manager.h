58b20502bc55ac03f08460882590e3663d5c362b
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-05-29 20:23:51 +0100

    rm com things

--- proj/src/model/com_manager/com_manager.h
+++ proj/src/model/com_manager/com_manager.h
@@ -10,29 +10,4 @@ typedef enum {
 	RECEIVING,
 } com_status_t;
 
-typedef struct {
-	uint8_t 	msg;
-	uint32_t 	timeout;
-
-	uint32_t 	current_timeout;
-	int				executed;
-	void 			(*onTimeout)(uint8_t, app_t *)
-} com_query_t;
-
-typedef enum {
-	NO_EVENT,
-	PING,
-	SEED
-} com_event_t;
-
-typedef struct {
-	com_status_t	status;
-
-	com_event_t 	current_ev;
-	queue_t 			*queries;
-	app_t					*app;
-} com_manager_t;
-
-com_manager_t *manager;
-
 #endif
