9c93523994bd3f360bd65de8bf041e77455eb90a
Marta Silva <up202208258@fe.up.pt>
2024-05-30 23:24:46 +0100

    documentation for drawer

--- proj/src/drawer/drawer.h
+++ proj/src/drawer/drawer.h
@@ -4,8 +4,37 @@
 #include "../drivers/drivers.h"
 #include "../model/app/app.h"
 
+/**
+ * @file drawer.h
+ * @brief Functions for drawing the game state and handling screen rendering.
+ */
+
+/**
+ * @brief Draws the current state of the application.
+ *
+ * This function draws the various elements of the game based on the current state stored in the app structure.
+ * 
+ * @param app Pointer to the application structure.
+ */
 void draw_state(app_t *app);
+
+/**
+ * @brief Draws the entire screen.
+ *
+ * This function first checks if a redraw is necessary. If it is, it draws the background, the current game state,
+ * the cursor, and then flips the screen buffer.
+ * 
+ * @param app Pointer to the application structure.
+ */
 void draw_screen(app_t *app);
+
+/**
+ * @brief Draws a set of buttons on the screen.
+ *
+ * This function iterates over the queue of buttons and draws each button at the specified position on the screen.
+ * 
+ * @param buttons Pointer to the queue of buttons to be drawn.
+ */
 void draw_button_set(queue_t *buttons);
 
 #endif
